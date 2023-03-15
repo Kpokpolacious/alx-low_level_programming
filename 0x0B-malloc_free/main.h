8ad566a2eb8f1baf1a6e0fea8db96b14bee3bae1
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-14 17:12:36 -0700

    U will see

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,11 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 void print_grid(int **grid, int width, int height);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_strdup(char *str);
-int main(void);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void print_grid(int **grid, int width, int height);
