7ddb3fb35afa6da6ef1cc84d05c56ec49267f308
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-16 17:29:37 -0700

    echo 0 showing 127

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - allocated memory uusing mallocc
+ * @b: size of memory to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,8 @@
+0x0C-more_malloc_free
+
+README
+
+General
+
+    How to use the exit function
+    What are the functions calloc and realloc from the standard library and how to use them.
