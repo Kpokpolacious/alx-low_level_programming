f944fcea23d2208e1c7b9c36e8fac6151c3fe930
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-16 18:16:26 -0700

    gcc have some issues believ me in C OR C standards should be modified

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
