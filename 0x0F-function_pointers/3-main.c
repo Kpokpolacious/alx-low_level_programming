2c8c31ada0a922f73fa0bf6a34ec53e7be121ee4
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-25 09:53:05 -0700

    Calcula

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,8 +10,6 @@
  * Return: Always 0.
  */
 
-
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
@@ -32,14 +30,11 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
 	printf("%d\n", get_op_func(op)(num1, num2));
-
 	return (0);
 }
