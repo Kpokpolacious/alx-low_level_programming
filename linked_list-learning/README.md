d5d3bcbc2a294f5cc2740ecc9fd041af4689e155
Kpokpolacious <nedolisaraphael@gmail.com>
2023-04-01 21:22:38 -0700

    ok

---
+++ linked_list-learning/README.md
@@ -0,0 +1,1 @@
+test-linked_list
---
+++ linked_list-learning/example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+void struct node node(const char *nodeptr, argc, argv[])
+
+typedef struct node *nodeptr
+{
+	node;
+	node.ptr;
+} node;
+
+//*nodeptr is used as node
+
+int main()
+{
+	firstnode;
+	node;
+	nodeptr;
+
+	ptr = malloc(sizeof(node));
+	node = 60;
+	
+	for (i = 0; (i+1) =< 60; i++)
+	where (i  = node.ptr)
+	{
+		printf("hello, kpokpo!\n");
+	}
+	return (0);
+
